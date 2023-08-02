2c0d1aac8b7bce85c238319e090210d4fd6b16a8
kazukikatoh <ryuglesh510@icloud.com>
2023-08-02 18:41:15 +0900

    no message

---
+++ AtlasManagementSystem-master/app/Http/Requests/SubCategoryRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SubCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'main_category_id' => 'required|exists:main_categories,id',
+            'sub_category_name' => 'required|string|max:100|unique:sub_categories,sub_category',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'main_category_id.required' => 'メインカテゴリーが選択されていません。',
+            'main_category_id.exists' => '選択したメインカテゴリーは存在しません。',
+            'sub_category_name.required' => 'サブカテゴリーは必ず入力してください。',
+            'sub_category_name.string' => 'サブカテゴリーは文字列で入力してください。',
+            'sub_category_name.max' => 'サブカテゴリーは100文字以内で入力してください。',
+            'sub_category_name.unique' => '指定したサブカテゴリーは既に存在します。',
+        ];
+    }
+}
