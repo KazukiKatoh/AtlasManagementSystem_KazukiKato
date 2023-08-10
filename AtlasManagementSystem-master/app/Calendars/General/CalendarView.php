ee79d948d4682ea96fe11e51560fe9a16fece0aa
kazukikatoh <ryuglesh510@icloud.com>
2023-08-10 14:39:07 +0900

    予約画面の表示形式を変更

--- AtlasManagementSystem-master/app/Calendars/General/CalendarView.php
+++ AtlasManagementSystem-master/app/Calendars/General/CalendarView.php
@@ -39,14 +39,14 @@ class CalendarView{
       foreach($days as $day){
         $startDay = $this->carbon->copy()->format("Y-m-01");
         $toDay = $this->carbon->copy()->format("Y-m-d");
-
+        // 過去日指定
         if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){
-          $html[] = '<td class="calendar-td">';
+          $html[] = '<td class="calendar-td " style="background-color: #D3D3D3;">';
         }else{
           $html[] = '<td class="calendar-td '.$day->getClassName().'">';
         }
         $html[] = $day->render();
-
+        // ログインしている人が予約している場合
         if(in_array($day->everyDay(), $day->authReserveDay())){
           $reservePart = $day->authReserveDate($day->everyDay())->first()->setting_part;
           if($reservePart == 1){
@@ -56,15 +56,21 @@ class CalendarView{
           }else if($reservePart == 3){
             $reservePart = "リモ3部";
           }
+          // 予約していてかつ過去日の場合
           if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){
-            $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px"></p>';
+            $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px">'. $reservePart .'参加'.'</p>';
             $html[] = '<input type="hidden" name="getPart[]" value="" form="reserveParts">';
           }else{
             $html[] = '<button type="submit" class="btn btn-danger p-0 w-75" name="delete_date" style="font-size:12px" value="'. $day->authReserveDate($day->everyDay())->first()->setting_reserve .'">'. $reservePart .'</button>';
             $html[] = '<input type="hidden" name="getPart[]" value="" form="reserveParts">';
           }
+          // 予約してない場合
         }else{
+          if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){
+          $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px">受付終了</p>';
+          }else{
           $html[] = $day->selectPart($day->everyDay());
+          }
         }
         $html[] = $day->getDate();
         $html[] = '</td>';
--- AtlasManagementSystem-master/resources/views/authenticated/bulletinboard/posts.blade.php
+++ AtlasManagementSystem-master/resources/views/authenticated/bulletinboard/posts.blade.php
@@ -8,6 +8,11 @@
     <div class="post_area border w-75 m-auto p-3">
       <p><span>{{ $post->user->over_name }}</span><span class="ml-3">{{ $post->user->under_name }}</span>さん</p>
       <p><a href="{{ route('post.detail', ['id' => $post->id]) }}">{{ $post->post_title }}</a></p>
+      @foreach($categories as $category)<div>
+
+      <span>{{ $category->subcategory }}</span>
+      </div>
+      @endforeach
       <div class="post_bottom_area d-flex">
         <div class="d-flex post_status">
           <div class="mr-5">
